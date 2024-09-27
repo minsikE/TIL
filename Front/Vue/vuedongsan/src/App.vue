f6926a6cffa8906c6303ea6acf0852235e628b1e
mslee <poweroverwhelming@jj.ac.kr>
2024-09-27 17:56:05 +0900

    [Vue.js] 모달창 만들기

--- Front/Vue/vuedongsan/src/App.vue
+++ Front/Vue/vuedongsan/src/App.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class="menu">
-    <a v-for="(a,i) in menus" :key="(i)">{{(i)}}</a>
+
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세 페이지</h4>
+      <p>상세 페이지 내용</p>
+    </div>
   </div>
-  원룸샵
-  <div v-for="(item,index) in items" :key="index">
-    
-    <h4>{{item.id}}</h4>
-    <p>{{(item.price)}} 만원</p>
+
+  <div class="menu">
+    <a v-for="(a,i) in menus" :key="(i)">{{(a)}}</a>
+  </div>
+  
+  <div>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{products[0]}}</h4>
+    <p>50 만원</p>
+    <button @click="신고수[0]++">허위매물신고</button> 
+    <span>신고수 : {{신고수[0]}}</span>
+  </div> 
+  <div>
+    <img src="./assets/room1.jpg" class="room-img">
+    <h4>{{products[1]}}</h4>
+    <p>60 만원</p>
+    <button @click="신고수[1]++">허위매물신고</button> 
+    <span>신고수 : {{신고수[1]}}</span>
+  </div>
+  <div>
+    <img src="./assets/room2.jpg" class="room-img">
+    <h4>{{products[2]}}</h4>
+    <p>70 만원</p>
+    <button @click="신고수[2]++">허위매물신고</button> 
+    <span>신고수 : {{신고수[2]}}</span>
   </div>
 </template>
 
@@ -16,12 +40,20 @@ export default {
   name: 'App',
   data(){
     return {
+      모달창열렸니 : false,
+      신고수 : [0,0,0],
       menus : ['Home', 'Shop', 'About'],
       items : [
-        {id : '역삼동 원룸', price: 50},
-        {id : '천호동 원룸', price: 70},
-        {id : '마포동 원룸', price: 90},
-      ],      
+        {id : '역삼동 원룸', price: 50, 신고수: 0},
+        {id : '천호동 원룸', price: 70, 신고수: 0},
+        {id : '마포동 원룸', price: 90, 신고수: 0},
+      ],
+      products : ['역삼동 원룸', '천호동 원룸', '마포동 원룸']      
+    }
+  },
+  methods : {
+    increase(index){            
+      this.items[index].신고수 += 1;
     }
   },
   components: {    
@@ -30,6 +62,28 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
